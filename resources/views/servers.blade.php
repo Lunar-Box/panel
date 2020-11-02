4734a492b6feff131457c1d378e79f2aebea3e5e
infoupgraders <infoupgraders@gmail.com>
2020-11-02 11:06:46 -0500

    Updated servers view page
    Updated servers view page so you can view it with a nice ui, and functions as it's supposed to

--- resources/views/servers.blade.php
+++ resources/views/servers.blade.php
@@ -17,124 +17,34 @@
 @endsection
 
 @section('body')
+{{-- {{ dd($servers) }} --}}
 <div style="margin-top: 50px;" class="row mr-md-20 mx-auto">
     <div class="col-md-8 offset-md-2 mx-auto">
         <div class="row">
-            {{-- @foreach($servers as $server)
+            @foreach($servers as $server)
                 <div class="col-sm-6">
-                    <div style="border-rad margin-bottom: 1.5rem;" class="bg-secondary card">
-                        <div style="border-rad background-color: #27343B; color: #fff;" class="card-body">
-                            <h5 class="card-title">{{ $server->name }}</h5>
+                    <div style="border-radius: 5px; margin-bottom: 1.5rem;" class="bg-secondary card">
+                        <div style="border-radius: 5px; background-color: #27343B; color: #fff;" class="card-body">
+                            <h5 class="card-title">{{ $server->uuid_short }}</h5>
                             <p class="card-text">Deployed&nbsp;{{ $server->created_at->diffForHumans() }}</p>
-                            @if($server->memory >= 1024)
+                            @if($server->memory > 1024)
                                 <i class="far fa-memory"></i>&nbsp;{{ $server->memory / 1000}}GB RAM<br>
                             @else
                                 <i class="far fa-memory"></i>&nbsp;{{ $server->memory}}MB RAM<br>
                             @endif
-                            <i class="far fa-microchip"></i>&nbsp;{{ $server->cpu }}%<br>
-                            @if($server->disk >= 1024)
+                            <i class="far fa-microchip"></i>&nbsp;{{ $server->cpu }}% CPU<br>
+                            @if($server->disk > 1024)
                                 <i class="far fa-hdd"></i>&nbsp;{{ $server->disk / 1000}}GB DISK<br>
                             @else
                                 <i class="far fa-hdd"></i>&nbsp;{{ $server->disk}}MB DISK<br>
                             @endif
+                            <i class="far fa-circle text-danger"></i>&nbsp;Offline<br>
                             </p>
-                            <a href="/server/{{ $server->id }}" class="btn bg-dark btn-secondary">View Server</a>
+                            <a href="/servers/view/{{  $server->id  }}" class="btn bg-dark btn-secondary">View Server</a>
                         </div>
                     </div>
                 </div>
-            @endforeach --}}
-
-            <div class="col-sm-6">
-                <div style="border-radius: 5px; margin-bottom: 1.5rem;" class="bg-secondary card">
-                    <div style="border-radius: 5px; background-color: #27343B; color: #fff;" class="card-body">
-                        <h5 class="card-title">Discord Bot</h5>
-                        <p class="card-text">Deployed&nbsp;17 seconds ago</p>
-                        {{-- @if($server->memory >= 1024) --}}
-                            {{-- <i class="far fa-memory"></i>&nbsp; $server->memory / 1000GB RAM<br> --}}
-                        {{-- @else --}}
-                            <i class="far fa-memory"></i>&nbsp;256MB RAM<br>
-                        {{-- @endif --}}
-                        <i class="far fa-microchip"></i>&nbsp;25% CPU<br>
-                        {{-- @if($server->disk >= 1024) --}}
-                            {{-- <i class="far fa-hdd"></i>&nbsp;$server->disk / 1000GB SSD<br> --}}
-                        {{-- @else --}}
-                            <i class="far fa-hdd"></i>&nbsp;512MB SSD<br>
-                        {{-- @endif --}}
-                        <i class="far fa-circle text-success"></i>&nbsp;Online<br>
-                        </p>
-                        <a href="/console" class="btn bg-dark btn-secondary">View Server</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-6">
-                <div style="border-radius: 5px; margin-bottom: 1.5rem;" class="bg-secondary card">
-                    <div style="border-radius: 5px; background-color: #27343B; color: #fff;" class="card-body">
-                        <h5 class="card-title">Discord Bot</h5>
-                        <p class="card-text">Deployed&nbsp;9 minutes ago</p>
-                        {{-- @if($server->memory >= 1024) --}}
-                            {{-- <i class="far fa-memory"></i>&nbsp; $server->memory / 1000GB RAM<br> --}}
-                        {{-- @else --}}
-                            <i class="far fa-memory"></i>&nbsp;512MB RAM<br>
-                        {{-- @endif --}}
-                        <i class="far fa-microchip"></i>&nbsp;25% CPU<br>
-                        {{-- @if($server->disk >= 1024) --}}
-                            {{-- <i class="far fa-hdd"></i>&nbsp;$server->disk / 1000GB SSD<br> --}}
-                        {{-- @else --}}
-                            <i class="far fa-hdd"></i>&nbsp;1GB SSD<br>
-                        {{-- @endif --}}
-                        <i class="far fa-circle text-danger"></i>&nbsp;Offline<br>
-                        </p>
-                        <a href="/console" class="btn bg-dark btn-secondary">View Server</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-6">
-                <div style="border-radius: 5px; margin-bottom: 1.5rem;" class="bg-secondary card">
-                    <div style="border-radius: 5px; background-color: #27343B; color: #fff;" class="card-body">
-                        <h5 class="card-title">Minecraft Server</h5>
-                        <p class="card-text">Deployed&nbsp;3 days ago</p>
-                        {{-- @if($server->memory >= 1024) --}}
-                            {{-- <i class="far fa-memory"></i>&nbsp; $server->memory / 1000GB RAM<br> --}}
-                        {{-- @else --}}
-                            <i class="far fa-memory"></i>&nbsp;1GB RAM<br>
-                        {{-- @endif --}}
-                        <i class="far fa-microchip"></i>&nbsp;50% CPU<br>
-                        {{-- @if($server->disk >= 1024) --}}
-                            {{-- <i class="far fa-hdd"></i>&nbsp;$server->disk / 1000GB SSD<br> --}}
-                        {{-- @else --}}
-                            <i class="far fa-hdd"></i>&nbsp;2GB SSD<br>
-                        {{-- @endif --}}
-                        <i class="text-warning far fa-circle"></i>&nbsp;Starting<br>
-                        </p>
-                        <a href="/console" class="btn bg-dark btn-secondary">View Server</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-6">
-                <div style="border-radius: 5px; margin-bottom: 1.5rem;" class="bg-secondary card">
-                    <div style="border-radius: 5px; background-color: #27343B; color: #fff;" class="card-body">
-                        <h5 class="card-title">Garry's Mod</h5>
-                        <p class="card-text">Deployed&nbsp;10 days ago</p>
-                        {{-- @if($server->memory >= 1024) --}}
-                            {{-- <i class="far fa-memory"></i>&nbsp; $server->memory / 1000GB RAM<br> --}}
-                        {{-- @else --}}
-                            <i class="far fa-memory"></i>&nbsp;2GB RAM<br>
-                        {{-- @endif --}}
-                        <i class="far fa-microchip"></i>&nbsp;100% CPU<br>
-                        {{-- @if($server->disk >= 1024) --}}
-                            {{-- <i class="far fa-hdd"></i>&nbsp;$server->disk / 1000GB SSD<br> --}}
-                        {{-- @else --}}
-                            <i class="far fa-hdd"></i>&nbsp;10GB SSD<br>
-                        {{-- @endif --}}
-                        <i class="text-success far fa-circle"></i>&nbsp;Online<br>
-                        </p>
-                        <a href="/console" class="btn bg-dark btn-secondary">View Server</a>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
