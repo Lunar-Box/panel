a389d072651d2cc7d82b1ceff92972747fe88326
infoupgraders <infoupgraders@gmail.com>
2020-11-02 11:04:35 -0500

    Added login check middleware

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Servers;
+use Illuminate\Support\Facades\Auth;
 
 class ViewController extends Controller
 {
-    public function index() {
-        return view('view');
+    public function index($id) {
+        $user = Auth::user();
+        $server = Servers::findOrFail($id);
+        if ($server->owner_id != Auth::user()->id) {
+            return abort(404);
+        };
+
+        return view('view')->with([
+            'server' => $server,
+        ]);
     }
 }
